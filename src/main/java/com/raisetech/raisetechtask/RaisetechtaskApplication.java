b9103f95a9ad09d5f8bd1a6430239f3a0afb8392
Hidenori Murai <hidenoweb822@gmail.com>
2022-10-15 07:59:01 +0900

    プロジェクト追加

---
+++ src/main/java/com/raisetech/raisetechtask/RaisetechtaskApplication.java
@@ -0,0 +1,13 @@
+package com.raisetech.raisetechtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaisetechtaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaisetechtaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raisetech/raisetechtask/RaisetechtaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raisetech.raisetechtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaisetechtaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
