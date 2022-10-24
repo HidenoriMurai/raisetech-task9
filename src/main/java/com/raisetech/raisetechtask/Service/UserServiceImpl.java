4cbdaf94574097349182d56c2c93cbe9154018a8
Hidenori Murai <hidenoweb822@gmail.com>
2022-10-25 08:25:30 +0900

    コンストラクタインジェクションに変更 フィールドインジェクションが非推奨だったため、 コンストラクタインジェクションに変更。

--- src/main/java/com/raisetech/raisetechtask/Service/UserServiceImpl.java
+++ src/main/java/com/raisetech/raisetechtask/Service/UserServiceImpl.java
@@ -2,7 +2,6 @@ package com.raisetech.raisetechtask.service;
 
 import com.raisetech.raisetechtask.entity.User;
 import com.raisetech.raisetechtask.mapper.UserMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,8 +9,12 @@ import java.util.List;
 @Service
 public class UserServiceImpl implements UserService {
 
-    @Autowired
-    private UserMapper userMapper;
+
+    private final UserMapper userMapper;
+    
+    public UserServiceImpl(UserMapper userMapper) {
+        this.userMapper = userMapper;
+    }
 
     @Override
     public List<User> findById(int id) {
