9fcdde1b87cdeb25d669eec79ca172b9721f1871
Hidenori Murai <hidenoweb822@gmail.com>
2022-10-25 15:25:13 +0900

    パスの変更 usersパス被りでの競合回避の修正

--- src/main/java/com/raisetech/raisetechtask/Controller/UserController.java
+++ src/main/java/com/raisetech/raisetechtask/Controller/UserController.java
@@ -2,14 +2,12 @@ package com.raisetech.raisetechtask.controller;
 
 import com.raisetech.raisetechtask.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
     //ユーザー情報サービス
@@ -17,20 +15,20 @@ public class UserController {
     private UserService userService;
 
     //ユーザー情報一覧
-    @GetMapping("/users")
+    @GetMapping("/")
     public List<UserResponse> getAll() {
 
         return userService.findAll().stream().map(UserResponse::new).toList();
     }
 
-    @GetMapping("/users/{id}")
+    @GetMapping("/{id}")
     public List<UserResponse> getUserOne(@PathVariable("id") int id) {
 
         return userService.findById(id).stream().map(UserResponse::new).toList();
     }
 
 
-    @GetMapping("/users/{age}")
+    @GetMapping
     public List<UserResponse> getUsers(@RequestParam("age") int age) {
 
         return userService.findByAge(age).stream().map(UserResponse::new).toList();
