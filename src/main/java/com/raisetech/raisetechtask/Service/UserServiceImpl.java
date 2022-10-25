7b598237debd4ca8a6a7b88b8c7384713437a93d
Hidenori Murai <hidenoweb822@gmail.com>
2022-10-25 13:46:49 +0900

    改行追加

--- src/main/java/com/raisetech/raisetechtask/Service/UserServiceImpl.java
+++ src/main/java/com/raisetech/raisetechtask/Service/UserServiceImpl.java
@@ -11,7 +11,7 @@ public class UserServiceImpl implements UserService {
 
 
     private final UserMapper userMapper;
-    
+
     public UserServiceImpl(UserMapper userMapper) {
         this.userMapper = userMapper;
     }
