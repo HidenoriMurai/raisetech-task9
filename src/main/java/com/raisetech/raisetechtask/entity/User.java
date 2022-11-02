a0e2e0b49873a46b3b56ed1abbd2b203a5b97511
Hidenori Murai <hidenoweb822@gmail.com>
2022-11-03 06:54:16 +0900

    不要なバリデーション削除

--- src/main/java/com/raisetech/raisetechtask/entity/User.java
+++ src/main/java/com/raisetech/raisetechtask/entity/User.java
@@ -1,20 +1,13 @@
 package com.raisetech.raisetechtask.entity;
 
 import lombok.Getter;
-import org.hibernate.validator.constraints.Range;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Positive;
 
 @Getter
 public class User {
-    @NotBlank
-    @Positive
     private final int id;
 
-    @NotBlank
     private String name;
-    @Range(min = 1, max = 130)
+
     private Integer age;
 
     public User(int id, String name, Integer age) {
